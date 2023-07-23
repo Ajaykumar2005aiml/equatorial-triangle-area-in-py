d8190f8ee58637fc4a96b60d1981156f32c4c792
Ajaykumar2005aiml <139782553+Ajaykumar2005aiml@users.noreply.github.com>
2023-07-23 15:49:03 +0530

    Create equat-tri.py
    The area of an equilateral triangle is ¼(√3a2) where "a" represents a side of the triangle. You are provided with the side "a". Find the area of the equilateral triangle.
    
    Input Description:
    The side of an equilateral triangle is provided as the input.
    
    Output Description:
    Find the area of the equilateral triangle and print the answer up to 2 decimal places after rounding off.
    
    Sample Input :
    20
    
    Sample Output :
    173.2

---
+++ equat-tri.py
@@ -0,0 +1,3 @@
+a=float(input()) #getting a float input
+ans=0.25*((3**0.5)*(a**2))#1/4=0.25,root of 3=3**0.5 and a**2.....this is the formula to find equatorial triangle area...
+print(round(ans,2))#finaly round a float to two values and than print
